bff6023b808fcc69be0379785bd48a5f21c28c61
stefan larsson <nbt12sln@student.hig.se>
2016-03-09 09:44:08 +0100

    .

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,50 +1,48 @@
 package shapes;
 
 public abstract class Shape implements Cloneable
-  {
-  protected Point position;
+{
+	protected Point position;
 
-  public Shape(double x, double y)
-    {
-    this(new Point(x, y));
-    }
+	public Shape(double x, double y)
+	{
+		this(new Point(x, y));
+	}
 
-  public Shape(Point p)
-    {
-    setPosition(p);
-    }
-  
-  public abstract void reDefine(double x, double y); // size, mm
-  
-  public Shape clone()
-    {
-    Shape result = null; 
-    try
-      {
-      result = (Shape)super.clone();
-      }
-    catch (CloneNotSupportedException e)
-      {
-      }
-    return result;
-    }
+	public Shape(Point p)
+	{
+		setPosition(p);
+	}
 
-  public Point getPosition()
-    {
-    return position; 
-    }
+	public abstract void reDefine(double x, double y); // size, mm
 
-  public void setPosition(double x, double y)
-    {
-    setPosition(new Point(x, y));
-    }
+	public Shape clone()
+	{
+		Shape result = null;
+		try
+		{
+			result = (Shape) super.clone();
+		} catch (CloneNotSupportedException e)
+		{
+		}
+		return result;
+	}
 
-  public void setPosition(Point p)
-    {
-    position = p;
-    }
+	public Point getPosition()
+	{
+		return position;
+	}
 
-  public abstract void accept(ShapeVisitor visitor);
-  
- 
-  }
+	public void setPosition(double x, double y)
+	{
+		setPosition(new Point(x, y));
+	}
+
+	public void setPosition(Point p)
+	{
+		position = p;
+	}
+
+	public abstract void accept(ShapeVisitor visitor);
+
+}
--- src/view/LemmingsApp.java
+++ src/view/LemmingsApp.java
@@ -1,14 +1,47 @@
 package view;
 
-public class LemmingsApp
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import model.DiggerScenario;
+import model.GameBoard;
+import model.Scenario;
+import shapes.TerrainList;
+
+public class LemmingsApp extends JFrame
 {
+	private static final long serialVersionUID = 1L;
 	
+	private Scenario diggerScenario = new DiggerScenario();
+	private GameBoard gameBoard = new GameBoard(diggerScenario);
+	private Container c;
+	private JPanel gamePanel, skillPanel, statusPanel;
+
+	public LemmingsApp()
+	{
+		super("Lemmings");
+		
+		gamePanel = createGamePanel();
+		skillPanel = createSkillPanel();
+		statusPanel = createStatusPanel();
+	}
 	
-	
-	
-	
-	
-	
+	private JPanel createGamePanel()
+	{		
+		JPanel temp = new JPanel();
+		temp.add(addTerrain(gameBoard.getTerrain()));
+		addLemmings();
+		return null;
+	}
+
+	private Shape addTerrain(TerrainList terrainList)
+	{
+		if(terrainList.size() != 0)
+			
+		
+	}
+
 	public static void main(String args[])
 	{
 		new LemmingsApp();
